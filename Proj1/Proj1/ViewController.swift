00f555cae88474505875c6fa7d88c3b2cfe0a601
sandeep <sandeep@sandeeps-MacBook-Air.local>
2023-10-18 19:41:34 +0530

    initial Commit

---
+++ Proj1/Proj1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Proj1
+//
+//  Created by sandeep on 2023-10-18.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
